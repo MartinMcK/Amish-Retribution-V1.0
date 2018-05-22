02c1d11d89b60f9fe4a628099c5091df16a7a7b4
MartinMcK <33032026+MartinMcK@users.noreply.github.com>
2018-05-22 13:40:16 +0100

    Initial commit

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject[] Enemies;
+    public GameObject GameOverObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (CheckIfAllDead())
+        {
+            GameOverObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        
+	}
+
+    bool CheckIfAllDead()
+    {
+        for(int i = 0; i < Enemies.Length; i++)
+        {
+            if(Enemies[i] != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerBehaviour : MonoBehaviour
+{
+    public delegate void UpdateHealth(int newHealth);
+    public static event UpdateHealth OnUpdateHealth;
+    public int health = 10;
+    void Start()
+    {
+        SendHealthData();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+  
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        SendHealthData();
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+    }
+    void SendHealthData()
+    {
+        if (OnUpdateHealth != null)
+        {
+            OnUpdateHealth(health);
+        }
+    }
+}
